eb0b387c420ed9d65d9196a4228bd340f26a5e95
Dannyyang0329 <loldanny329@gmail.com>
2025-04-30 20:49:50 +0800

    add socketio

--- webhw/asgi.py
+++ webhw/asgi.py
@@ -8,25 +8,22 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import socketio
+import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-import frontend.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webhw.settings')
+django.setup()
 
 # 初始化Django ASGI應用
 django_asgi_app = get_asgi_application()
 
-# 設定ASGI應用
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                frontend.routing.websocket_urlpatterns
-            )
-        )
-    ),
-})
+# 導入Socket.IO事件處理器
+from frontend.socketio_handlers import sio
+
+# 創建ASGI應用
+application = socketio.ASGIApp(
+    sio,
+    django_asgi_app,
+    socketio_path="socket.io"
+)
